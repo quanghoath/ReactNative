6bee4db573c145d28d893d158c5eb1c3940bb0d1
Nguyen Hoa <hoanqfpt@gmail.com>
2021-06-27 10:39:32 +0700

    NoiDung

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.textStyle}>HomeScreen1</Text>
       <Button
-        onPress={() => navigation.navigate('Components')}
+        onPress={() => navigation.navigate("Components")}
         title="Go to component screen"
       />
       <Button
-      onPress={()=> navigation.navigate('List')}
-      title="Go to list screen"
+        onPress={() => navigation.navigate("List")}
+        title="Go to list screen"
       />
-       <Button
-      onPress={()=> navigation.navigate('Image')}
-      title="Go to image screen"
+      <Button
+        onPress={() => navigation.navigate("Image")}
+        title="Go to image screen"
+      />
+      <Button
+        onPress={() => navigation.navigate("Counter")}
+        title="Go to counter screen"
       />
       {/* <TouchableOpacity
         onPress={() => {
